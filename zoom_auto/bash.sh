e25267ba6c8d2bede65a33157355c997b3cde63b
hari261104 <72310702+hari261104@users.noreply.github.com>
2021-07-03 15:20:28 +0530

    Add files via upload

---
+++ zoom_auto/bash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DISPLAY=:0
+cd 
+python3 main.py
---
+++ zoom_auto/code.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+#add the url , i.e., the invite link of the zoom meeting between the ' '
+url = ' '
+
+#opening url in default browser
+webbrowser.open(url)
